9b511f807a62ddf78c1e3e5f6af28804c8f9812a
Marla Blanco <m_blanco444@yahoo.com>
2023-08-31 14:50:55 -0700

    files_added

---
+++ static/js/logic.js
@@ -0,0 +1,80 @@
+//create map
+let myMap = L.map('map').setView([40.73, -74.0059], 3);
+
+//add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+const url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+//call the api with d3
+d3.json(url).then(function(data){
+  //leaflet layer group
+  let earthquakes = L.layerGroup();
+    data.features.forEach(function(feature) {
+    //coordinates of the earthquake
+    let coordinates = feature.geometry.coordinates;
+    let depth = coordinates[2];
+    let lat = coordinates[1];
+    let lng = coordinates[0];
+    
+    
+    //magnitude of the earthquake
+    let magnitude = feature.properties.mag;
+    
+    //create circle marker for the earthquake
+    let marker = L.circleMarker([lat, lng], {
+      radius: magnitude * 3,
+      color: '#000',
+      weight: 1,
+      fillColor: getColor(depth),
+      fillOpacity: 0.7
+    });
+    
+    //add a popup to the marker with information about the earthquake
+    marker.bindPopup(`<strong>Location:</strong> ${feature.properties.place}<br>
+      <strong>Magnitude:</strong> ${magnitude}<br>
+      <strong>Depth:</strong> ${depth} km`);
+    
+    //add marker to layer group
+    marker.addTo(earthquakes);
+  });
+  
+  //add the layer group to the map
+  earthquakes.addTo(myMap);
+
+//create a legend control
+let legend = L.control({position: 'bottomright'});
+
+//add the legend to the map
+legend.onAdd = function () {
+  let div = L.DomUtil.create('div', 'info legend'),
+      depths = [-10, 10, 30, 50, 70, 90],
+      labels = [];
+
+  //generate a label with a colored square for each interval
+  for (let i = 0; i < depths.length; i++) {
+    labels.push(
+        '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
+        depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+')
+  );
+}
+div.innerHTML=labels.join('');
+
+return div;
+};
+  //function to get the color based on the depth of the earthquake
+  function getColor(d) {
+    return d > 90 ? '#800026' :
+           d > 70 ? '#BD0026' :
+           d > 50 ? '#E31A1C' :
+           d > 30 ? '#FC4E2A' :
+           d > 10 ? '#FD8D3C' :
+           d > -10 ? '#FEB24C':
+           '#FFFFCC';
+}
+
+legend.addTo(myMap);
+
+});
